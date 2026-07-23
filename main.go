a688789ea77af76155849b63c04686f6a4ac403f
KAijook <hoangtung41125@gmail.com>
2026-07-23 10:32:42 +0700

    Mutiple result

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 )
 
-func add(x int, y int) int {
-	return x + y
+func swap(x, y string) (string, string) {
+	return y, x
 }
-
 func main() {
-	fmt.Println(add(3, 2))
+	a, b := swap("Hello", "World")
+	fmt.Println(a, b)
 }
