2e0dbbeab1f9db7ecebba1e89c5e05786a3633e3
KAijook <hoangtung41125@gmail.com>
2026-07-23 10:49:08 +0700

    for

--- main.go
+++ main.go
@@ -2,13 +2,31 @@ package main
 
 import "fmt"
 
-const Pi = 3.14
-
 func main() {
-	const World = "世界"
-	fmt.Println("Hello", World)
-	fmt.Println("Happy", Pi, "Day")
 
-	const Truth = true
-	fmt.Println("Go rules?", Truth)
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	for j := 0; j < 3; j++ {
+		fmt.Println(j)
+	}
+
+	for i := range 3 {
+		fmt.Println("range", i)
+	}
+
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	for n := range 6 {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
 }
